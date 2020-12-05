bf281a2d6736eea8573080717e89bc6d7242264c
Emanuel Trandafir <emanueltrandafir@yahoo.com>
2020-12-05 17:26:43 +0200

    added some styles to the index page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import flask
+from flask import request
 
 app = flask.Flask(__name__)
 
@@ -12,6 +13,19 @@ def is_alive():
 def index():
     return flask.render_template('index.html')
 
+
+@app.route('/upload_csv', methods=['POST'])
+def upload_file():
+
+        print(123)
+        # csvfile = request.files['sensor_data.csv']
+        file = request.files['file']
+        if file:
+            print('**found file', file.filename)
+
+
+        # reader = csv.DictReader(csvfile)
+        # data = [row for row in reader]
 
 if __name__ == '__main__':
     app.run()
