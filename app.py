d5421b904b30e0a2026f3fd6a0aeef736f842526
Emanuel Trandafir <emanueltrandafir@yahoo.com>
2020-12-05 15:20:22 +0200

    added index page

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import flask
 
-from flask import Flask
-app = Flask(__name__)
+app = flask.Flask(__name__)
+
 
 @app.route('/isAlive')
-def hello_world():
+def is_alive():
     return 'sensor-data-reader is alive!'
+
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()
